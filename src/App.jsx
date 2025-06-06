12d2c48d525de05d99a5416f547011c849f2b4ae
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-06-06 19:23:43 +0200

    Add product list / Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
-export default function App() {}
+import products from "./array";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Prodotti</h1>
+      <ul>
+        {products.map((product) => (
+          <li>
+            {product.name} - €{product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
